19646260a263a4568b4b99ea6b5e71fce2ad293e
Agustín Nieto <anietog1@eafit.edu.co>
2020-03-17 12:20:13 -0500

    Subiendo solución con el chown. Hay que ver si los permisos dañan el server

--- runner-register.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-TOKEN= # insertar el token
-PARAMS="--non-interactive --url https://gitlab.com/ --registration-token $TOKEN --executor shell"
-while ! docker exec -it runner gitlab-runner register $PARAMS; do
-    sleep 1
-done
--- setup.sh
+++ setup.sh
@@ -1,4 +1,11 @@
 #! /bin/bash
 
 docker-compose up -d
-./runner-register.sh
+
+TOKEN= # insertar el token
+PARAMS="--non-interactive --url https://gitlab.com/ --registration-token $TOKEN --executor shell"
+while ! docker exec -it runner gitlab-runner register $PARAMS; do
+    sleep 1
+done
+
+docker exec -it runner chown -R gitlab-runner:gitlab-runner /bitnami
